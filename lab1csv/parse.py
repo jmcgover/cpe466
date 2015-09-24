dba061c9c81bac180368fec8d646273d27570c5e
Jeff McGovern <jeffreydmcgovern@gmail.com>
2015-09-24 14:42:39 -0700

    rough basic implementation of reading files nad counting paragraphs

--- lab1csv/parse.py
+++ lab1csv/parse.py
@@ -2,16 +2,32 @@
 import sys
 import os
 
+def 
+
 def main():
     print 'hello'
 
     # Initial input checking
     if len(sys.argv) < 2:
         print("Insufficient arguments. Usage: parse.py <fileName>")
-        sys.exit()
+        sys.exit(22)
     if os.path.isfile(sys.argv[1]) == False:
         print("File does not exist.")
-        sys.exit()
+        sys.exit(22)
+    if sys.argv[1][-3:] != "txt":
+        print("Bad file extenstion.")
+        sys.exit(22)
+    file = open(sys.argv[1], 'r')
+    lines = file.readlines()
+    file.close()
+    paragraphCount = 0
+    for line in lines:
+        if line == '\n':
+            print("Found a new line!")
+            paragraphCount += 1
+    if paragraphCount:
+        paragraphCount += 1
+    print("Paragraph Count: %d" % (paragraphCount))
     return 0
 
 if __name__ == '__main__':

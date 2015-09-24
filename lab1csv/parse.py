f09cb89c86e915061caf750600e1e0d1e8e93559
Nicole Martin <nlmartin8@gmail.com>
2015-09-24 14:12:56 -0700

    Added param and file existance checks

--- lab1csv/parse.py
+++ lab1csv/parse.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python
+import sys
+import os
 
 def main():
     print 'hello'
+
+    # Initial input checking
+    if len(sys.argv) < 2:
+        print("Insufficient arguments. Usage: parse.py <fileName>")
+        sys.exit()
+    if os.path.isfile(sys.argv[1]) == False:
+        print("File does not exist.")
+        sys.exit()
     return 0
 
 if __name__ == '__main__':

f1a387b8078b24c31ba99529f07cf45da7317ee0
Nicole Martin <nlmartin8@gmail.com>
2015-09-28 23:55:27 -0700

    Added detection for csv files

--- lab1csv/parse.py
+++ lab1csv/parse.py
@@ -19,19 +19,23 @@ def main():
     if os.path.isfile(sys.argv[1]) == False:
         print("File does not exist.")
         sys.exit(22)
-    if sys.argv[1][-3:] != "txt":
+    if sys.argv[1][-3:] == "txt":
+        doc = Document()
+        with open(sys.argv[1]) as file:
+            parser = document.Parser(file, doc)
+            parser.parseDocument()
+        print("Total Words         : %d" % (doc.getNumTotalWords()))
+        print("Different Words     : %d" % (doc.getNumDifferentWords()))
+        print("Number of Sentences : %d" % (doc.getNumSentences()))
+        print("Number of Paragraphs: %d" % (doc.getNumParagraphs()))
+        for w in doc:
+            print("%s: %s" % (w, doc.getWordCount(w)))
+    elif sys.argv[1][-3:] == "csv":
+        print("CSV file extenstion.")
+        sys.exit(22)
+    else:
         print("Bad file extenstion.")
         sys.exit(22)
-    doc = Document()
-    with open(sys.argv[1]) as file:
-        parser = document.Parser(file, doc)
-        parser.parseDocument()
-    print("Total Words         : %d" % (doc.getNumTotalWords()))
-    print("Different Words     : %d" % (doc.getNumDifferentWords()))
-    print("Number of Sentences : %d" % (doc.getNumSentences()))
-    print("Number of Paragraphs: %d" % (doc.getNumParagraphs()))
-    for w in doc:
-        print("%s: %s" % (w, doc.getWordCount(w)))
 
 if __name__ == '__main__':
     main()

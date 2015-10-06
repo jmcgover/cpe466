468cf1a52863c272a4666a6e672a29b64f9cda2c
jmcgover <jmcgover@calpoly.edu>
2015-10-06 12:30:15 -0700

    adjusted indentation spaces and file check
    Spaces for indentation were for some reason 3 spaces. Changed it back to
    4, which is what Python needs.
    
    Adjusted how we check files, so that we aren't just checking the
    extension, but ensuring that the json parser can actually load the file.

--- lab2csv/run.py
+++ lab2csv/run.py
@@ -17,27 +17,29 @@ import json
 DESCRIPTION="CPE 466 Lab 2: Information Retrieval from Digital Democracy."
 
 def buildArguments():
-   argParser = argparse.ArgumentParser(prog=sys.argv[0], description=DESCRIPTION)
-   argParser.add_argument('-f','--file',
-         action='store',
-         metavar='file',
-         help='the file to be parsed, appended with .json for JSON and .???? for the ???',
-         required=True)
+    argParser = argparse.ArgumentParser(prog=sys.argv[0], description=DESCRIPTION)
+    argParser.add_argument('-f','--file',
+           action='store',
+           metavar='file',
+           help='the file to be parsed, appended with .json for JSON and .???? for the ???',
+           required=True)
 
-   return argParser
+    return argParser
 
 def main():
-   argParser = buildArguments()
-   args = argParser.parse_args()
-   filename = args.file
-   print("----------")
-   if filename[-4:] == 'json':
-      print('Okay json')
-      return 0
-   else:
-      print('Add support for the stored, processed json.')
-      return 22
+    argParser = buildArguments()
+    args = argParser.parse_args()
+    filename = args.file
+    if filename[-4:] != 'json':
+        print('This better be a json file!')
+    with open(filename) as corpus:
+        try:
+            json.load(corpus)
+        except ValueError as err:
+            print('Failed to load %s as json file' % filename)
+            return 22
+        print('Sucessfully loaded %s' % filename)
+    return 0
 
 if __name__ == '__main__':
-   main()
-
+    main()

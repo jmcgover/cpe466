65b5f264f59d6a20be7b072916d90733f2aa58eb
Jeff McGovern <jmcgover@calpoly.edu>
2015-10-20 11:49:47 -0700

    basic hello world to test package importing

---
+++ lab3pagerank/run.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+# CPE 466 Fall 2015
+# Jeff McGovern - jmcgover@calpoly.edu
+# Nicole Martin - nlmartin@calpoly.edu
+
+import errno
+import json
+import os
+import pickle
+import sys
+import textwrap
+import argparse
+
+def main():
+    print("Hello World")
+
+if __name__ == '__main__':
+    rtn = main()
+    exit(rtn)

79d278dc537e0e10730231bfbe832694b9da4108
jmcgover <jmcgover@calpoly.edu>
2015-10-21 09:30:33 -0700

    added opening file and basic structure of graph

--- lab3pagerank/graph.py
+++ lab3pagerank/graph.py
@@ -1,5 +1,74 @@
+import bisect
 
 class Graph(object):
-    def __init__(self, nodes, edges):
-        self.nodes = nodes
-        self.edges = edges
+    def __init__(self):
+        self.nodes = {}
+        self.edges = []
+
+    def addEdge(self, node, edgeLabel, neighbor):
+        if a not in self.nodes:
+            self.nodes[node] = {}
+        self.nodes[node].addEdge(neighbor, edgeLabel)
+        self.edges.append(Edge(node, edgeLabel, neighbor))
+    def containsNode(self, node):
+        return node in self.nodes
+
+class Parser(object):
+    def __init__(self, file, csv=True, gml=False, graph=None):
+        if not csv and not gml:
+            raise AttributeError('Must be either csv or gml, not neither.')
+        if csv and gml:
+            raise AttributeError('Must be either csv or gml, not both.')
+        self.file = file
+        self.csv = csv
+        self.gml = gml
+        self.graph = None
+    def parseGraph(self):
+        try:
+            self.file.readable()
+        except IOError as err:
+            raise  IOError('File is not readable. Please ensure that it is open.')
+        if not self.graph:
+            self.graph = Graph()
+        if self.csv:
+            # CSV Parsing goes here
+            print('will parse CSV')
+        if self.gml:
+            # GML Parsing goes here
+            print('will parse GML')
+        return self.graph
+
+class Node(object):
+    def __init__(self, label):
+        self.label = label
+        self.edges = {}
+        self.neighbors = []
+
+    def hash(self):
+        return self.label.hash()
+
+    def addEdge(self, neighbor, edgeLabel):
+        bisect.insort(self.neighbors, neighbor)
+        self.edges[neighbor] = edgeLabel
+
+    def getLabel(self):
+        return self.label
+    def getEdge(self, neighbor):
+        return self.neighbors[neighbor]
+    def getEdges(self):
+        return self.edges
+    def getNeighbors(self):
+        return self.neighbors
+
+class Edge(object):
+    def __init__(self, node, label, neighbor):
+        self.node = node
+        self.label = label
+        self.neighbor = neighbor
+
+    def getNode(self):
+        return self.node
+    def getLabel(self):
+        return self.label
+    def getNeighbor(self):
+        return self.neighbor
--- lab3pagerank/run.py
+++ lab3pagerank/run.py
@@ -14,9 +14,10 @@ import textwrap
 import argparse
 
 sys.path.append(os.getcwd())
+from graph import Edge
 from graph import Graph
 from graph import Node
-from graph import Edge
+from graph import Parser
 
 WIDTH=100
 INDENT=4
@@ -24,10 +25,47 @@ DESCRIPTION="CPE 466 Lab 3: PageRank."
 
 def buildPageRankArgs():
     argParser = argparse.ArgumentParser(prog=sys.argv[0], description=DESCRIPTION)
+    argParser.add_argument('-f','--filename',
+            action='store',
+            metavar='filename',
+            help='the file to be parsed, appended with .csv for Comma-Separated Value or .gml for Graph Markup Language',
+            required=True)
+    group = argParser.add_mutually_exclusive_group()
+    group.add_argument('-c', '--csv',
+            action='store_true',
+            default=False,
+            help='forces Comma-Separated Value file interpretation')
+    group.add_argument('-g', '--gml',
+            action='store_true',
+            default=False,
+            help='forces Graph Markup Language file interpretation')
     return argParser
 
 def main():
     argParser = buildPageRankArgs()
+    args = argParser.parse_args()
+    # FIGURE OUT FILETYPE
+    if not args.csv and not args.gml:
+        args.csv  = args.filename[-4:] == '.csv'
+        args.gml  = args.filename[-4:] == '.gml'
+        if not args.csv and not args.gml:
+            print('Please provide a file ending in .csv or .gml \
+                    or provide the appropriate command line flags')
+            argParser.print_help()
+            return errno.EINVAL
+
+    # PARSE GRAPH
+    graph = None
+    try:
+        graphParser = None
+        with open(args.filename) as raw_graph_file:
+            print('Parsing %s' % (args.filename))
+            graphParser = Parser(raw_graph_file, csv=args.csv, gml=args.gml)
+            graph = graphParser.parseGraph()
+    except OSError as e:
+        if e.errno == errno.ENOENT:
+            print('Could not find graph file "%s"' % (args.filename))
+            return errno.ENOENT
 
 if __name__ == '__main__':
     rtn = main()

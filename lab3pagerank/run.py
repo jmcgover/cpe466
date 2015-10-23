60987995090515dc94f27204b2ac5575feed570c
jmcgover <jmcgover@calpoly.edu>
2015-10-23 10:01:17 -0700

    addedd stats and an initial node count arg

--- lab3pagerank/run.py
+++ lab3pagerank/run.py
@@ -19,6 +19,10 @@ from graph import Graph
 from graph import Node
 from graph import Parser
 
+from pagerank import PageRank
+from pagerank import PageRankCalculator
+from pagerank import PageRankResults
+
 WIDTH=100
 INDENT=4
 DESCRIPTION  = 'CPE 466 Lab 3: PageRank.'
@@ -43,6 +47,19 @@ def buildPageRankArgs():
             action='store_true',
             default=False,
             help='prints the graph as a single pseudo-JSON object')
+    argParser.add_argument('-S', '--print-stats',
+            action='store_true',
+            default=False,
+            help='prints numebrs relevant to the graph')
+    # Initial size of graph
+    argParser.add_argument('-i', '--initial-size',
+            action='store',
+            type=int,
+            nargs='?',
+            metavar='nodes',
+            required=False,
+            help='number of nodes to initialize the matrix with')
+    # Sub Group of Filetype
     group = argParser.add_mutually_exclusive_group()
     group.add_argument('-c', '--csv',
             action='store_true',
@@ -77,10 +94,10 @@ def main():
     graph = None
     try:
         graphParser = None
-        with open(args.filename) as raw_graph_file:
+        with open(args.filename, 'r') as raw_graph_file:
             if not quiet:
                 print('Parsing %s...' % (args.filename))
-            graphParser = Parser(raw_graph_file, csv=args.csv, gml=args.gml, txt=args.txt)
+            graphParser = Parser(raw_graph_file, csv=args.csv, gml=args.gml, txt=args.txt, quiet=args.quiet, initialSize=args.initial_size)
             graph = graphParser.parseGraph()
             if not quiet:
                 print('Done parsing %s.' % (args.filename))
@@ -88,6 +105,9 @@ def main():
         if e.errno == errno.ENOENT:
             print('Could not find graph file "%s"' % (args.filename))
             return errno.ENOENT
+        else:
+            print('Some other error occured with %s' % (args.filename))
+            raise e
     if graph == None:
         print('Soemthing fucky happened and graph is empty', file=sys.stderr)
         return errno.EINVAL
@@ -99,18 +119,27 @@ def main():
         if not quiet:
             print('-' * WIDTH)
             print('Printing NODES')
+            print('-' * WIDTH)
         for n in graph:
             print(n)
-        if not quiet:
-            print('-' * WIDTH)
     # GRAPH
     if args.print_graph:
         if not quiet:
             print('-' * WIDTH)
             print('Printing GRAPH')
+            print('-' * WIDTH)
         print(graph)
+    if args.print_stats:
         if not quiet:
             print('-' * WIDTH)
+            print('Printing STATS')
+            print('-' * WIDTH)
+        print("Nodes: %d" % (graph.getNumNodes()))
+        print("Edges: %d" % (graph.getNumEdges()))
+    if not quiet:
+        print('-' * WIDTH)
+
+    # PAGE RANK
 
 if __name__ == '__main__':
     rtn = main()

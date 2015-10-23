06c676fb98fd1d350a0cb3abc30c8396a462c3e3
jmcgover <jmcgover@calpoly.edu>
2015-10-23 15:58:10 -0700

    results aren't correct

--- lab3pagerank/pagerank.py
+++ lab3pagerank/pagerank.py
@@ -4,6 +4,7 @@
 # Nicole Martin - nlmartin@calpoly.edu
 
 
+import bisect
 import gc
 import os
 import sys
@@ -14,16 +15,17 @@ from graph import Node
 from graph import Parser
 
 class PageRank(object):
-    def __init__(self, nodeLabel, edges, pageRank):
+    def __init__(self, nodeLabel, pageRank):
         self.nodeLabel = nodeLabel
-        self.edges = edges
         self.pageRank = pageRank
     def __hash__(self):
         return self.nodeLabel.__hash__()
+    def __cmp__(self, other):
+        return self.pageRank.__cmp__(other.pageRank)
     def __lt__(self, other):
-        return self.pageRank < other.pageRank
+        return self.pageRank > other.pageRank
     def __str__(self):
-        return "%s: %f" % (self.nodeLabel, self.pageRank)
+        return "%s\t%f" % (self.nodeLabel, self.pageRank)
     def getEdges(self):
         return self.edges
     def getNodeLabel(self):
@@ -37,30 +39,37 @@ class PageRankResults(object):
         self.numNodes = graph.getNumNodes()
         self.pageRanks = dict.fromkeys(graph.getNodesUsed())
         self.maxDelta = None
+        self.iterations = 0
         for node in self.pageRanks:
-            self.pageRanks[node] = PageRank(node, self.graph.getNodeEdges(node), 1.0/self.numNodes)
-        print(self.pageRanks)
-    def __iter__(self):
-        return self.pageRanks.__iter__()
+            self.pageRanks[node] = PageRank(node, 1.0/self.numNodes)
 
     def updatePageRanks(self, d):
         newPageRanks = dict.fromkeys(self.pageRanks)
         pageRank = None
         self.maxDelta = 0
         for node in self.graph.getNodesUsed():
-            pageRank = (1 - d) / self.numNodes
+            newPageRank = (1-d)/self.numNodes
             neighbors = self.graph.getNodeNeighbors(node)
             for neighbor in neighbors:
-                d * self.pageRanks[neighbor].getPageRank() / len(neighbors)
-            newPageRanks[node] = PageRank(node, self.graph.getNodeEdges(node), pageRank)
-            self.maxDelta = max(self.maxDelta, (newPageRanks[node].getPageRank() - self.pageRanks[node].getPageRank()))
+                newPageRank += d * self.pageRanks[neighbor].getPageRank()
+            newPageRanks[node] = PageRank(node, newPageRank);
+            oldPageRank = self.pageRanks[node].getPageRank()
+            self.maxDelta = max(self.maxDelta, abs(newPageRank - oldPageRank))
         self.pageRanks = newPageRanks
+        self.iterations += 1
         gc.collect()
 
     def getMaxDelta(self):
         return self.maxDelta
+    def getIterations(self):
+        return self.iterations
     def getPageRank(self, node):
         return self.pageRanks[node]
+    def getPageRanks(self):
+        sortedRanks = []
+        for node,rank in self.pageRanks.items():
+            bisect.insort(sortedRanks, rank)
+        return sortedRanks
 
 class PageRankCalculator(object):
     def __init__(self, graph):

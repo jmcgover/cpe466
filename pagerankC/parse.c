37e311a779f6a823b8233a434da096f0315ab0da
jmcgover <jmcgover@calpoly.edu>
2015-10-29 13:49:29 -0700

    adjusted bugs

--- pagerankC/parse.c
+++ pagerankC/parse.c
@@ -1,33 +1,10 @@
 #include "parse.h"
 
-#include <stdio.h>
 #include <errno.h>
+#include <stdlib.h>
 
 #define TRUE  1
 #define FALSE 0
-
-typedef struct args args_t;
-struct args {
-    char *graph_filename;
-};
-int parse_args(int argc, char **argv);
-void print_usage(int argc, char **argv, char *msg);
-int print_help(FILE *stream, int argc, char **argv);
-
-/*
- * Runs the main sequence of instructions
- */
-int main(int argc, char **argv) {
-    int rtn;
-    args_t args;
-
-    rtn = parse_args(argc, argv, &args);
-    if (rtn) {
-        return rtn;
-    }
-    fprintf(stdout, "Hello, World!\n");
-    return 0;
-}
 
 /*
  * \brief Parses the arguments passed in from CLI
@@ -71,14 +48,14 @@ int print_usage(FILE *stream, int argc, char **argv, char *msg) {
  * @param argv   char ** pointer to the array of string arguments
  */
 #define OPT_WIDTH 10
-int print_opt(File *stream, char opt_short, char *opt_long, char *description) {
+int print_opt(FILE *stream, char opt_short, char *opt_long, char *description) {
     if (opt_short && opt_long) {
         fprintf(stream, "-%c, --%-*s %s", opt_short, OPT_WIDTH, opt_long, description);
     } else {
         if (opt_short) {
             fprintf(stream, "-%c %-*s %s", opt_short, OPT_WIDTH, " ", description);
         } else if (opt_long) {
-            fprintf(stream, "%-*s %s", opt_short, OPT_WIDTH, opt_long, description);
+            fprintf(stream, "%-*s %s", OPT_WIDTH, opt_long, description);
         } else {
         }
     }
--- pagerankC/parse.h
+++ pagerankC/parse.h
@@ -1,4 +1,15 @@
 #ifndef PARSE_H
 #define PARSE_H
 
+#include <stdio.h>
+
+typedef struct args args_t;
+struct args {
+    char *graph_filename;
+};
+int parse_args(int argc, char **argv, args_t *args);
+void print_usage(int argc, char **argv, char *msg);
+int print_help(FILE *stream, int argc, char **argv);
+
+
 #endif /* PARSE_H */

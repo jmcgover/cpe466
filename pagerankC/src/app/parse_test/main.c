87a922f5c297e79409a75be89dbd56506ed33693
jmcgover <jmcgover@calpoly.edu>
2015-10-31 03:58:25 -0700

    tests parsing of arguments

--- pagerankC/src/app/parse_test/main.c
+++ pagerankC/src/app/parse_test/main.c
@@ -1,16 +1,19 @@
-#include "../../lib/argparse/argparse.h"
+#include "../../lib/arg_parse/arg_parse.h"
 
 /*
  * Runs the main sequence of instructions
  */
 int main(int argc, char **argv) {
     int rtn;
-    args_t args;
+    args_t args = {0};
 
     rtn = parse_args(argc, argv, &args);
-    if (rtn) {
+    if (rtn || args.help_printed) {
         return rtn;
     }
+    printf("Graph Filename: %s\n", args.graph_filename);
+    print_usage(stdout, argc, argv, NULL);
+    print_help(stderr, argc, argv);
     fprintf(stdout, "Hello, World!\n");
     return 0;
 }

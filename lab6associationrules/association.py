4a8885b9b631fd4531c4bcc4049e1d0dda3d15c7
Jeff McGovern <jmcgover@calpoly.edu>
2015-12-04 18:24:07 -0800

    added data for association rules mining

---
+++ lab6associationrules/association.py
@@ -0,0 +1,18 @@
+#! /usr/local/bin/python3
+
+# CPE 466 Fall 2015
+# Lab 6: Association Rule Mining
+# Jeff McGovern - jmcgover@calpoly.edu
+# Nicole Martin - nlmartin@calpoly.edu
+
+import errno
+import os
+import sys
+
+def main():
+
+   return 0
+
+if __name__ == '__main__':
+   rtn = main()
+   sys.exit(rtn)
